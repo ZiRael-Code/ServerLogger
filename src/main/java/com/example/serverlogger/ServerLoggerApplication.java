dd6991c9392ef05d26a6a0191df02d5a417bf57a
Israel <oladejoisreal@gmail.com>
2025-05-02 15:00:16 +0100

    commit

--- src/main/java/com/example/serverlogger/ServerLoggerApplication.java
+++ src/main/java/com/example/serverlogger/ServerLoggerApplication.java
@@ -3,13 +3,16 @@ package com.example.serverlogger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+@EnableScheduling
 @SpringBootApplication
 public class ServerLoggerApplication {
 
@@ -17,4 +20,16 @@ public class ServerLoggerApplication {
         SpringApplication.run(ServerLoggerApplication.class, args);
     }
 
+    private static final String API_URL = "https://serverlogger-h77l.onrender.com/logger/getLastLog/TestLog";
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Scheduled(fixedRate = 10 * 60 * 1000)
+    public void callApiEvery10Minutes() {
+        try {
+            System.out.println("in call api meod: " + java.time.LocalDateTime.now());
+            restTemplate.getForObject(API_URL, String.class);
+            System.out.println("API called successfully at: " + java.time.LocalDateTime.now());
+        } catch (Exception e) {
+            System.err.println("Failed to call API: " + e.getMessage());
+        }
+    }
 }
